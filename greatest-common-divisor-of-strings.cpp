7a8d9a439c572fe0f5ceda2fb6949e7847914e93
shivangi31 <shivangidhakad31@gmail.com>
2020-07-02 17:25:43 +0200

    Update greatest-common-divisor-of-strings.cpp

--- greatest-common-divisor-of-strings.cpp
+++ greatest-common-divisor-of-strings.cpp
@@ -33,3 +33,21 @@ public:
         return "";
     }
 };
+
+
+
+//The above is the solution I came up with
+//This is an optimized solution.
+/*
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1.size() < str2.size()) return gcdOfStrings(str2, str1);
+        if (str2.empty()) return str1;
+        if (str1.substr(0, str2.size()) != str2) return "";
+        return gcdOfStrings(str1.substr(str2.size()), str2);
+    }
+};
+
+*/
